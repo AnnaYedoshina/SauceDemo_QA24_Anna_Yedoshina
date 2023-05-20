2c632681bc61c54d7d5bb9734a4165c4415445a6
AnnaYedoshina <yedoshina.java@gmail.com>
2023-05-20 11:02:06 +0300

    New pages and tests added

--- src/test/java/Tests/CheckOutTests.java
+++ src/test/java/Tests/CheckOutTests.java
@@ -30,6 +30,6 @@ public class CheckOutTests extends BaseTest {
         shoppingCartPage.clickCheckOutButton();
         checkOutPage.clickCancelButton();
         Assert.assertTrue(shoppingCartPage.isContinueShoppingIsDisplayed());
-        shoppingCartPage.clickCheckOutButton();
+
     }
 }
